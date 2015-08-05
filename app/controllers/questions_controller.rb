ef72a4abdc6dd8ca43003ef9c36c4e73c84058f3
Akitek <keith@cloud9>
2015-08-05 03:35:10 +0000

    Added Delete Questions feature

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -48,7 +48,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+      format.html { redirect_to @poll, notice: 'Question was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
