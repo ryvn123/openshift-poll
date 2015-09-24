742963c0bedd53cadbf39f096ac06284002723bf
wykeith <wykeith@gmail.com>
2015-09-24 15:39:30 +0000

    Tweak Stats page

--- app/lib/poll_serializer.rb
+++ app/lib/poll_serializer.rb
@@ -7,7 +7,7 @@ class PollSerializer
 
       {
         data: data,
-        title: "Polls answered by month",
+        title: "Number of collected Polls by month",
         x_axis: {
           legend: "Polls per month",
           series: replies.keys.map { |date| date.strftime("%b %Y") }
@@ -22,17 +22,18 @@ class PollSerializer
     def answers_per_question question
       answers_per_question = question.answers.group_by(&:possible_answer)
       data = answers_per_question.map { |possible_answer, answers| answers.length }
-      series = answers_per_question.map { |possible_answer, answers| (possible_answer.nil? ? 'No Answer' : possible_answer.try(:title)) }
+      series = answers_per_question.map { |possible_answer, answers| (possible_answer.nil? ? 'No Answer' : possible_answer.check? ? '[+]:' + possible_answer.try(:title) + ' [Correct Ans]': possible_answer.try(:title))  }
+      colors = answers_per_question.map { |possible_answer, answers| (possible_answer.nil? ? 'blue' : possible_answer.check? ? 'red' : ' blue')  }
       {
         data: data,
         title: question.title,
         x_axis: {
-          legend: "Polls per month",
-          series: series
+          legend: "Replies",
+          series: series,
         },
         y_axis: {
           legend: "No. polls",
-          scale: [0, (data.max or 0) + 1]
+          scale: [0, (data.max or 0) + 1],
         }
       }
     end
